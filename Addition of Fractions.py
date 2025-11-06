6827bbd757165c6c9ed94ba2d7882b8500edc89b
jakebeep00 <jaegeunKim.work@gmail.com>
2025-11-06 12:09:07 +0900

    Addition of Fractions

--- Addition of Fractions.py
+++ Addition of Fractions.py
@@ -3,7 +3,7 @@ import math
 def solution(numer1, denom1, numer2, denom2):
     num = (numer1*denom2) + (numer2*denom1)
     den = (denom1*denom2)
-    gcd = math.gcd(num,den)
+    gcd = math.gcd(num,den) #최대공약수를 구하는 방식
     
     fin_num = num / gcd
     fin_den = den / gcd
